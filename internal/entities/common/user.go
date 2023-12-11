1dc71a8cef18ad279d51de326eaaf5c9e5ba10ea
Dmitry <dmitry@goverland.xyz>
2023-12-11 18:26:09 +0300

    Add user avatars for proposals and voters

--- internal/entities/common/user.go
+++ internal/entities/common/user.go
@@ -1,9 +1,49 @@
 package common
 
+import "fmt"
+
+const (
+	avatarSizeXS = 16 * 2
+	avatarSizeS  = 32 * 2
+	avatarSizeM  = 64 * 2
+	avatarSizeL  = 128 * 2
+)
+
+var (
+	avatarTemplate = "https://cdn.stamp.fyi/avatar/%s?s=%d"
+)
+
 type UserAddress string
+
+type UserAvatar struct {
+	Size string `json:"size"`
+	Link string `json:"link"`
+}
+type UserAvatars []UserAvatar
 
 type User struct {
 	Address      UserAddress `json:"address"`
 	ResolvedName *string     `json:"resolved_name"`
-	Avatar       *string     `json:"avatar"`
+	Avatars      UserAvatars `json:"avatars"`
+}
+
+func GenerateUserAvatars(address string) UserAvatars {
+	return UserAvatars{
+		{
+			Size: "xs",
+			Link: fmt.Sprintf(avatarTemplate, address, avatarSizeXS),
+		},
+		{
+			Size: "s",
+			Link: fmt.Sprintf(avatarTemplate, address, avatarSizeS),
+		},
+		{
+			Size: "m",
+			Link: fmt.Sprintf(avatarTemplate, address, avatarSizeM),
+		},
+		{
+			Size: "l",
+			Link: fmt.Sprintf(avatarTemplate, address, avatarSizeL),
+		},
+	}
 }

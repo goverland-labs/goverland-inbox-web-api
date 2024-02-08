86e803060f21fdffd97f704e866ec33f7d552d49
Alexander Kirii <nwn.wert@gmail.com>
2024-02-08 17:15:20 +0500

    Add me can vote endpoint

--- internal/rest/server.go
+++ internal/rest/server.go
@@ -98,6 +98,7 @@ func NewServer(
 	handler.HandleFunc("/me", srv.getMe).Methods(http.MethodGet).Name("auth_get_me")
 	handler.HandleFunc("/me", srv.deleteMe).Methods(http.MethodDelete).Name("auth_delete_me")
 	handler.HandleFunc("/me/votes", srv.getUserVotes).Methods(http.MethodGet).Name("get_user_votes")
+	handler.HandleFunc("/me/can-vote", srv.getMeCanVote).Methods(http.MethodGet).Name("get_me_can_vote")
 
 	handler.HandleFunc("/dao", srv.listDAOs).Methods(http.MethodGet).Name("get_dao_list")
 	handler.HandleFunc("/dao/top", srv.listTopDAOs).Methods(http.MethodGet).Name("get_dao_top")
--- internal/rest/server_user.go
+++ internal/rest/server_user.go
@@ -2,14 +2,17 @@ package rest
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	coresdk "github.com/goverland-labs/core-web-sdk"
+	"github.com/goverland-labs/inbox-api/protobuf/inboxapi"
+	"github.com/rs/zerolog/log"
+
 	"github.com/goverland-labs/inbox-web-api/internal/auth"
 	internaldao "github.com/goverland-labs/inbox-web-api/internal/entities/dao"
 	"github.com/goverland-labs/inbox-web-api/internal/entities/proposal"
 	"github.com/goverland-labs/inbox-web-api/internal/helpers"
-	"github.com/rs/zerolog/log"
-	"net/http"
 
 	"github.com/goverland-labs/inbox-web-api/internal/appctx"
 	"github.com/goverland-labs/inbox-web-api/internal/rest/request"
@@ -116,6 +119,88 @@ func (s *Server) getUserVotes(w http.ResponseWriter, r *http.Request) {
 
 	response.AddPaginationHeaders(w, r, offset, limit, resp.TotalCnt)
 	response.SendJSON(w, http.StatusOK, &proposalWithVotes)
+}
+
+func (s *Server) getMeCanVote(w http.ResponseWriter, r *http.Request) {
+	session, ok := appctx.ExtractUserSession(r.Context())
+	if !ok {
+		w.WriteHeader(http.StatusUnauthorized)
+
+		return
+	}
+
+	proposals, err := s.userClient.GetUserCanVoteProposals(r.Context(), &inboxapi.GetUserCanVoteProposalsRequest{
+		UserId: session.UserID.String(),
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("get user can vote proposals")
+
+		response.SendEmpty(w, http.StatusInternalServerError)
+		return
+	}
+
+	proposalList, err := s.coreclient.GetProposalList(r.Context(), coresdk.GetProposalListRequest{
+		ProposalIDs: proposals.GetProposalIds(),
+		Limit:       len(proposals.GetProposalIds()),
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("get proposal list")
+
+		response.SendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// todo: use caching for getting dao
+	daoIds := make([]string, 0)
+	for _, info := range proposalList.Items {
+		daoIds = append(daoIds, info.DaoID.String())
+	}
+	daolist, err := s.daoService.GetDaoList(r.Context(), internaldao.DaoListRequest{
+		IDs:   daoIds,
+		Limit: len(daoIds),
+	})
+	if err != nil {
+		log.Error().Err(err).Msg("get dao list by IDs")
+
+		response.SendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	daos := make(map[string]*internaldao.DAO)
+	for _, info := range daolist.Items {
+		daos[info.ID.String()] = info
+	}
+
+	list := make([]proposal.Proposal, len(proposalList.Items))
+	for i, info := range proposalList.Items {
+		di, ok := daos[info.DaoID.String()]
+		if !ok {
+			log.Error().Msg("dao not found")
+
+			response.SendError(w, http.StatusBadRequest, fmt.Sprintf("dao not found: %s", info.DaoID))
+			return
+		}
+		list[i] = convertProposalToInternal(&info, di)
+	}
+
+	list = s.enrichProposalsVotesInfo(r.Context(), session, list)
+	list = helpers.WrapProposalsIpfsLinks(list)
+
+	proposalsWithoutVotes := make([]proposal.Proposal, 0, len(list))
+	for _, p := range list {
+		if p.UserVote != nil {
+			continue
+		}
+
+		proposalsWithoutVotes = append(proposalsWithoutVotes, p)
+	}
+
+	log.Info().
+		Int("count_filtered", len(proposalsWithoutVotes)).
+		Int("total", proposalList.TotalCnt).
+		Msg("me can vote")
+
+	response.SendJSON(w, http.StatusOK, &proposalsWithoutVotes)
 }
 
 func (s *Server) getUserAddress(session auth.Session) (address string, exist bool) {
